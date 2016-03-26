c500ac4c3a0f477b51c99c0091f1692a48b9929e
RenzHoly <renzholy@gmail.com>
2016-03-27 03:30:05 +0800

    fix NPE in clickLink

--- app/src/main/java/pw/bits/weisper/view/widget/StatusTextView.java
+++ app/src/main/java/pw/bits/weisper/view/widget/StatusTextView.java
@@ -8,7 +8,6 @@ import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 
-import pw.bits.weisper.WeisperApplication;
 import pw.bits.weisper.event.OpenTopicEvent;
 import pw.bits.weisper.event.OpenUserEvent;
 import pw.bits.weisper.library.WeiboTextView;
@@ -17,6 +16,8 @@ import pw.bits.weisper.library.WeiboTextView;
  * Created by rzh on 16/3/26.
  */
 public class StatusTextView extends WeiboTextView {
+    private static Context context;
+
     private static Clickable clickUser = new Clickable() {
         @Override
         public void onClick(String value) {
@@ -38,26 +39,27 @@ public class StatusTextView extends WeiboTextView {
         public void onClick(String value) {
             Log.i("click link", value);
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(value));
-            WeisperApplication.instance.startActivity(browserIntent);
+            context.startActivity(browserIntent);
         }
     };
 
     public StatusTextView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public StatusTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public StatusTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
-    private void init() {
+    private void init(Context context) {
+        StatusTextView.context = context;
         setClickUser(clickUser);
         setClickTopic(clickTopic);
         setClickLink(clickLink);
