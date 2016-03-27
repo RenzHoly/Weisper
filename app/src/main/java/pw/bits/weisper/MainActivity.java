044bc407aead1c116a17b53f2ed5b62bf9edf38a
RenzHoly <renzholy@gmail.com>
2016-03-27 15:27:00 +0800

    add ActionBarDrawerToggle

--- app/src/main/java/pw/bits/weisper/MainActivity.java
+++ app/src/main/java/pw/bits/weisper/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -45,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer_layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,6 +83,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupView() {
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer_layout, toolbar, 0, 0);
+        drawer_layout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
