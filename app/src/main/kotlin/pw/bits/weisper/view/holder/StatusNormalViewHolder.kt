943c842fa317d3b6b29e2ab1955558330def6a3c
RenzHoly <renzholy@gmail.com>
2016-03-31 23:17:47 +0800

    refactor

--- app/src/main/kotlin/pw/bits/weisper/view/holder/StatusNormalViewHolder.kt
+++ app/src/main/kotlin/pw/bits/weisper/view/holder/StatusNormalViewHolder.kt
@@ -35,12 +35,16 @@ class StatusNormalViewHolder(itemView: View, parentWidth: Int) : StatusAbstractV
     internal var status_attitudes_count = itemView.findViewById(R.id.status_attitudes_count) as TextView
     internal var status_attitudes_icon = itemView.findViewById(R.id.status_attitudes_icon) as ImageView
     internal var status_pictures_container = itemView.findViewById(R.id.status_pictures_container) as ThumbnailsLayout
-    internal var retweeted_status = itemView.findViewById(R.id.retweeted_status)
+    internal var retweeted_status_layout = itemView.findViewById(R.id.retweeted_status_layout)
     internal val parentWidth = parentWidth
 
     override fun bindView(status: Status) {
+        val user = status.user
+        val retweeted_status = status.retweeted_status
+        val retweeted_status_user = retweeted_status.user
+
         status_text.text = status.text
-        user_screen_name.text = status.user.screen_name
+        user_screen_name.text = user.screen_name
         if (status.source.length > 0) {
             status_source.text = String.format("%s 来自 %s", PrettyTime(Locale.CHINA).format(status.created_at).replace(" ", ""), Html.fromHtml(status.source))
         } else {
@@ -51,16 +55,16 @@ class StatusNormalViewHolder(itemView: View, parentWidth: Int) : StatusAbstractV
         setStatusInfo(status_comments_icon, status_comments_count, status.comments_count!!)
         setStatusInfo(status_attitudes_icon, status_attitudes_count, status.attitudes_count!!)
 
-        retweeted_status.visibility = if (status.retweeted_status == null) View.GONE else View.VISIBLE
-        if (status.retweeted_status != null) {
-            if (status.retweeted_status.user != null) {
-                retweeted_status_text.text = String.format("@%s:%s", status.retweeted_status.user.screen_name, status.retweeted_status.text)
+        retweeted_status_layout.visibility = if (retweeted_status == null) View.GONE else View.VISIBLE
+        if (retweeted_status != null) {
+            if (retweeted_status_user != null) {
+                retweeted_status_text.text = String.format("@%s:%s", retweeted_status_user.screen_name, retweeted_status.text)
             } else {
                 retweeted_status_text.text = "抱歉，此微博已被作者删除"
             }
         }
 
-        user_profile_image.setUser(status.user)
+        user_profile_image.setUser(user)
 
         status_layout.setOnClickListener { v ->
             val popup = StatusPopupMenu(itemView.context, status_attitudes_count, Gravity.END)
@@ -68,16 +72,16 @@ class StatusNormalViewHolder(itemView: View, parentWidth: Int) : StatusAbstractV
             popup.show()
         }
 
-        if (status.retweeted_status != null) {
-            retweeted_status.setOnClickListener { v ->
+        if (retweeted_status != null) {
+            retweeted_status_layout.setOnClickListener { v ->
                 val popup = StatusPopupMenu(itemView.context, status_attitudes_count, Gravity.END)
-                popup.setStatus(status.retweeted_status)
+                popup.setStatus(retweeted_status)
                 popup.show()
             }
         }
 
         val lp = itemView.layoutParams as ViewGroup.MarginLayoutParams
-        status_pictures_container.setPictures(if (status.retweeted_status == null) status.pic_urls else status.retweeted_status.pic_urls, parentWidth - lp.leftMargin - lp.rightMargin)
+        status_pictures_container.setPictures(if (retweeted_status == null) status.pic_urls else retweeted_status.pic_urls, parentWidth - lp.leftMargin - lp.rightMargin)
     }
 
     private fun setStatusInfo(icon: ImageView, text: TextView, count: Int) {
