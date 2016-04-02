6e2ee8316dbb157eb1d5ef3eeb61c35a5d8d7c64
RenzHoly <renzholy@gmail.com>
2016-04-02 11:37:05 +0800

    add LeakCanary

--- app/src/main/java/pw/bits/weisper/WeisperApplication.java
+++ app/src/main/java/pw/bits/weisper/WeisperApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by rzh on 16/3/27.
@@ -17,5 +18,6 @@ public class WeisperApplication extends Application {
                 .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
                 .build());
         Logger.init().logLevel(LogLevel.FULL);
+        LeakCanary.install(this);
     }
 }
