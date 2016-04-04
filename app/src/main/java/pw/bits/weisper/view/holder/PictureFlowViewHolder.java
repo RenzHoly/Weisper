f4face1fee73e14d96fde8e8832af96d1fdbabc1
RenzHoly <renzholy@gmail.com>
2016-04-04 12:58:25 +0800

    bind picture flow

--- app/src/main/java/pw/bits/weisper/view/holder/PictureFlowViewHolder.java
+++ app/src/main/java/pw/bits/weisper/view/holder/PictureFlowViewHolder.java
@@ -7,24 +7,16 @@ import android.view.View;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.util.List;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import pw.bits.weisper.BR;
 import pw.bits.weisper.R;
 import pw.bits.weisper.event.OpenUserEvent;
-import pw.bits.weisper.library.bean.Picture;
 import pw.bits.weisper.library.bean.Status;
-import pw.bits.weisper.view.image.PictureFlowLayout;
 
 /**
  * Created by rzh on 16/3/19.
  */
 public class PictureFlowViewHolder extends RecyclerView.ViewHolder {
-    @Bind(R.id.picture_flow_layout)
-    PictureFlowLayout picture_flow_layout;
-
     private int parentWidth = 0;
 
     public PictureFlowViewHolder(View itemView, int parentWidth) {
@@ -38,16 +30,8 @@ public class PictureFlowViewHolder extends RecyclerView.ViewHolder {
         Handlers handlers = new Handlers(status);
         binding.setVariable(BR.status, status);
         binding.setVariable(BR.handlers, handlers);
+        binding.setVariable(BR.width, parentWidth);
         binding.executePendingBindings();
-
-        List<Picture> pictures = status.retweeted == null ? status.pictures : status.retweeted.pictures;
-
-        itemView.setVisibility(pictures.size() == 0 ? View.GONE : View.VISIBLE);
-        if (pictures.size() == 0) {
-            itemView.getLayoutParams().height = 0;
-        }
-
-        picture_flow_layout.setPictures(pictures, parentWidth);
     }
 
     public class Handlers {
--- app/src/main/java/pw/bits/weisper/view/image/PictureFlowLayout.java
+++ app/src/main/java/pw/bits/weisper/view/image/PictureFlowLayout.java
@@ -1,11 +1,11 @@
 package pw.bits.weisper.view.image;
 
 import android.content.Context;
+import android.databinding.BindingAdapter;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.LinearLayout;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import pw.bits.weisper.library.bean.Picture;
@@ -14,43 +14,29 @@ import pw.bits.weisper.library.bean.Picture;
  * Created by rzh on 16/3/21.
  */
 public class PictureFlowLayout extends LinearLayout {
-    private List<PictureFlowImageView> pictureViews = new ArrayList<>();
-
     public PictureFlowLayout(Context context) {
         super(context);
-        init(context);
     }
 
     public PictureFlowLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
     }
 
     public PictureFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
     }
 
-    private void init(Context context) {
-        for (int i = 0; i < 9; i++) {
-            PictureFlowImageView imageView = new PictureFlowImageView(context);
+    @BindingAdapter({"pictures", "width"})
+    public static void setPictures(PictureFlowLayout view, @Nullable List<Picture> pictures, int width) {
+        view.removeAllViews();
+        for (int i = 0; i < (pictures == null ? 0 : pictures.size()); i++) {
+            PictureFlowImageView imageView = new PictureFlowImageView(view.getContext());
             imageView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
             imageView.setAdjustViewBounds(true);
-            addView(imageView);
-            pictureViews.add(imageView);
-        }
-    }
-
-    public void setPictures(final List<Picture> pictures, int maxHeight) {
-        for (int i = 0; i < pictures.size(); i++) {
-            final int position = i;
-            pictureViews.get(i).setVisibility(View.VISIBLE);
-            pictureViews.get(i).setImage(pictures, position);
-            pictureViews.get(i).setMaxHeight(maxHeight);
-        }
-        for (int i = pictures.size(); i < 9; i++) {
-            pictureViews.get(i).setVisibility(View.GONE);
-            pictureViews.get(i).setImageBitmap(null);
+            imageView.setImage(pictures, i);
+            imageView.setMaxHeight(width);
+            imageView.setMinimumHeight(width / 2);
+            view.addView(imageView);
         }
     }
 }
