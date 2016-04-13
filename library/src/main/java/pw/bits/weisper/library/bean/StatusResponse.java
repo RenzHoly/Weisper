38bb65053cef28f3668193a2a9c897839a871973
RenzHoly <renzholy@gmail.com>
2016-04-13 23:10:46 +0800

    fix StatusResponse

--- library/src/main/java/pw/bits/weisper/library/bean/StatusResponse.java
+++ library/src/main/java/pw/bits/weisper/library/bean/StatusResponse.java
@@ -1,5 +1,9 @@
 package pw.bits.weisper.library.bean;
 
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,17 +11,19 @@ import java.util.List;
  * Created by rzh on 16/3/15.
  */
 public class StatusResponse {
-    List<Status> items = new ArrayList<>();
+    @SerializedName("statuses")
+    List<Status> items;
 
+    @NonNull
     public List<Status> getItems() {
-        return items;
+        return items == null ? new ArrayList<>() : items;
     }
 
-    public Long getSinceId() {
+    public long getSinceId() {
         return items.size() == 0 ? 0L : items.get(0).id;
     }
 
-    public Long getMaxId() {
+    public long getMaxId() {
         return items.size() == 0 ? 0L : items.get(items.size() - 1).id - 1;
     }
 }
