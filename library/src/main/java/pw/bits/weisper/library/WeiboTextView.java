ffebf54463561d3586bd9bdac75a50de912a6a1e
RenzHoly <renzholy@gmail.com>
2016-04-14 00:15:39 +0800

    refactor WeiboLinkMovementMethod

--- library/src/main/java/pw/bits/weisper/library/WeiboTextView.java
+++ library/src/main/java/pw/bits/weisper/library/WeiboTextView.java
@@ -7,8 +7,12 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
 import android.text.method.MovementMethod;
+import android.text.method.Touch;
+import android.text.style.BackgroundColorSpan;
 import android.text.style.ClickableSpan;
 import android.text.style.DynamicDrawableSpan;
 import android.text.style.ImageSpan;
@@ -50,7 +54,7 @@ public class WeiboTextView extends TextView {
     }
 
     private void init() {
-        setMovementMethod(WeiboMovementMethod.instance);
+        setMovementMethod(WeiboLinkMovementMethod.instance);
     }
 
     @Override
@@ -166,9 +170,53 @@ public class WeiboTextView extends TextView {
     public boolean onTouchEvent(MotionEvent event) {
         boolean value = super.onTouchEvent(event);
         MovementMethod method = getMovementMethod();
-        if (method instanceof WeiboMovementMethod) {
-            return ((WeiboMovementMethod) method).isLink();
+        if (method instanceof WeiboLinkMovementMethod) {
+            return ((WeiboLinkMovementMethod) method).isLink();
         }
         return value;
+    }
+
+    static class WeiboLinkMovementMethod extends LinkMovementMethod {
+        public static final WeiboLinkMovementMethod instance = new WeiboLinkMovementMethod();
+        private static BackgroundColorSpan graySpan = new BackgroundColorSpan(0xAAAAAA);
+        private static boolean isLink = false;
+
+        @Override
+        public boolean onTouchEvent(TextView widget, Spannable buffer, MotionEvent event) {
+            isLink = false;
+            int action = event.getAction();
+            if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                x -= widget.getTotalPaddingLeft();
+                y -= widget.getTotalPaddingTop();
+                x += widget.getScrollX();
+                y += widget.getScrollY();
+                int line = widget.getLayout().getLineForVertical(y);
+                int offset = widget.getLayout().getOffsetForHorizontal(line, x);
+                ClickableSpan[] spans = buffer.getSpans(offset, offset, ClickableSpan.class);
+                if (spans.length != 0) {
+                    int start = buffer.getSpanStart(spans[0]);
+                    int end = buffer.getSpanEnd(spans[0]);
+                    isLink = true;
+                    if (action == MotionEvent.ACTION_DOWN) {
+                        buffer.setSpan(graySpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    } else {
+                        spans[0].onClick(widget);
+                        buffer.removeSpan(graySpan);
+                    }
+                    return true;
+                }
+            } else {
+                buffer.removeSpan(graySpan);
+            }
+            return Touch.onTouchEvent(widget, buffer, event);
+        }
+
+        public boolean isLink() {
+            boolean value = isLink;
+            isLink = false;
+            return value;
+        }
     }
 }
